6d39b3e91a9ed7c40c2416733bc29aacc5c2c65d
yamini465 <40388091+yamini465@users.noreply.github.com>
2020-07-09 16:43:28 +0530

    Updates

---
+++ precache-manifest.c56803f9191eb9aa1d1ae637aa08daac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56ac378454554e1a427f6c59fd2d2a96",
+    "url": "/loginregister/index.html"
+  },
+  {
+    "revision": "976698a5a28568c3aa17",
+    "url": "/loginregister/static/css/main.d37c3440.chunk.css"
+  },
+  {
+    "revision": "2197fff8349db30ab862",
+    "url": "/loginregister/static/js/2.8acad2e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/loginregister/static/js/2.8acad2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "976698a5a28568c3aa17",
+    "url": "/loginregister/static/js/main.be893438.chunk.js"
+  },
+  {
+    "revision": "5dad896aefeeedfba68f",
+    "url": "/loginregister/static/js/runtime-main.5dc89700.js"
+  }
+]);
